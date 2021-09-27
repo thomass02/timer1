d48fbd7a5011409ab001cac17c8ce55bc45a55a2
thomass02 <jrt6v4@gmail.com>
2021-09-26 21:47:19 -0500

    countdown ICP
    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+//main class
+export class AppComponent implements OnInit{
   title = 'timer1';
+
+  // default constructor
+  constructor() {
+  }
+
+  // placeholder for "on initiation"
+  ngOnInit(): void {
+  }
+
+  //minDate = new Date();
+  //maxDate = new Date(2015,1,1);
+
+  start: any;
+  // hard coded time for testing
+  countDown = new Date("September 30, 2021 17:20:00").getTime();
+
+  //event array for testing changes made to datepicker
+  events: string[] = [];
+
+  addEvent(type: string, event: MatDatepickerInputEvent<unknown, unknown | null>) {
+    this.events.push(`${type}: ${event.value}`);
+    //let eventDate = new Date(this.events.push(`${event.value}`));
+    //console.log(eventDate);
+    console.log(this.events.push(`${event.value}`));
+  }
+
+  // convert event time into days, hours, min and seconds
+  timer:any;
+  x = setInterval(()=>{
+    var now = new Date().getTime();
+    var interval = this.countDown - now;
+    var days = Math.floor(interval/(1000*60*60*24));
+    var hours = Math.floor((interval % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((interval % (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((interval % (1000*60)) / 1000);
+    this.timer = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+
+    //for testing
+    //console.log(now);
+    //console.log(this.countDown);
+
+    // if interval duration is negative, the event is passed - display
+    if(interval < 0){
+      clearInterval(this.x);
+      this.timer= 'The event has passed'
+    }
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import {NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule,
+    {provide: MAT_DATE_LOCALE, useValue: 'en-US'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
